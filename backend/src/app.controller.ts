bdb7543fe421db4c82cf566a9c8ee538ad8b64bc
zahra shahab <zshahab91@gmail.com>
2025-07-20 21:42:27 +0200

    create first api for boards

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('start')
+  start(): string {
+    return this.appService.start();
+  }
+  @Get('board/:id')
+  getBoard(@Param('id') id: string): any {
+    const dbPath = path.join(__dirname, '..', 'db', 'chess.json');
+    let boardsData: any[];
+    try {
+      const data = fs.readFileSync(dbPath, 'utf8');
+      boardsData = JSON.parse(data);
+    } catch (error) {
+      console.error('Error reading chess data:', error);
+      return { error: 'Error reading chess data' };
+    }
+    console.log('Boards Data:', boardsData);
+    if (!Array.isArray(boardsData)) {
+      return { error: 'Invalid chess data format' };
+    }
+    const board = boardsData.find((b: { id: string }) => b.id === id);
+    if (!board) {
+      return { error: 'Board not found' };
+    }
+    return board;
   }
 }
