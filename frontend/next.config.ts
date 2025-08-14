b6335241a1f9eb9880abcc5c63bfc2e7b9209a2c
zahra shahab <zshahab91@gmail.com>
2025-08-14 11:42:10 +0200

    Fixed2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   /* config options here */
+  experimental: {
+    /* add supported experimental options here */
+  }
 };
 
 export default nextConfig;
