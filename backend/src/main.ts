e65eb2c251f3008d0f10f115a75936d0ef0dd324
zahra shahab <zshahab91@gmail.com>
2025-08-15 21:04:51 +0200

    change CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:4000", // or "*" for all origins (not recommended for production)
+    origin: "*", // or "*" for all origins (not recommended for production)
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
